916c188325f21932b1b0f58a901514002ea0d6ff
pierrots <pierrot.sylvain14@gmail.com>
2022-12-17 15:42:21 +0100

    refactor: remove useless files

--- back-end/server.js
+++
@@ -1,7 +0,0 @@
-const app = require("./src/app");
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => {
-  console.log(`Server is connected on ${port}`);
-});
--- back-end/src/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express-promise-router")();
-const authController = require("../controllers/auth.controller");
-
-router.post("/login", authController.loginArchitect);
-router.post("/logout", authController.deleteRefreshTokenArchitect);
-router.post("/refresh", authController.refreshTokenArchitect);
-
-module.exports = router;
