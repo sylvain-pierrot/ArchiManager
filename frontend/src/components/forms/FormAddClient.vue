1cfc1474fcd4cb2ab68c71cd0594ee0bbe5f0f4a
pierrots <pierrot.sylvain14@gmail.com>
2023-01-02 21:56:05 +0100

    feat(frontend): row extension clients table

--- frontend/src/components/forms/FormAddClient.vue
+++ frontend/src/components/forms/FormAddClient.vue
@@ -51,7 +51,9 @@
           placeholder="Email"
           flat
           class="col-6"
-          :rules="[(val) => (val && val.length > 0) || 'Ce champs est requis']"
+          :rules="[
+            (val) => (val && isValidEmail(val)) || 'Ce champs est requis',
+          ]"
           lazy-rules
         />
 
@@ -142,6 +144,12 @@ const client = ref({
   telephone: "",
   notes: "",
 });
+
+function isValidEmail(val) {
+  const emailPattern =
+    /^(?=[a-zA-Z0-9@._%+-]{6,254}$)[a-zA-Z0-9._%+-]{1,64}@(?:[a-zA-Z0-9-]{1,63}\.){1,8}[a-zA-Z]{2,63}$/;
+  return emailPattern.test(val);
+}
 
 function isValidPhoneNumber(phoneNumber) {
   const regex = /^\d{2}-\d{2}-\d{2}-\d{2}-\d{2}$/;
--- frontend/src/components/tabs/TabClients.vue
+++ frontend/src/components/tabs/TabClients.vue
@@ -22,7 +22,43 @@
             <FormAddClient @client="emitClient" />
           </q-dialog>
 
-          <q-table :rows="rows" :columns="columns" row-key="id" flat bordered />
+          <q-table :rows="rows" :columns="columns" row-key="name" flat bordered>
+            <template v-slot:header="props">
+              <q-tr :props="props">
+                <q-th auto-width />
+                <q-th v-for="col in props.cols" :key="col.name" :props="props">
+                  {{ col.label }}
+                </q-th>
+              </q-tr>
+            </template>
+
+            <template v-slot:body="props">
+              <q-tr :props="props">
+                <q-td auto-width>
+                  <q-btn
+                    size="sm"
+                    color="warning"
+                    round
+                    dense
+                    @click="props.expand = !props.expand"
+                    :icon="props.expand ? 'remove' : 'add'"
+                  />
+                </q-td>
+                <q-td v-for="col in props.cols" :key="col.name" :props="props">
+                  {{ col.value }}
+                </q-td>
+              </q-tr>
+              <q-tr v-show="props.expand" :props="props">
+                <q-td colspan="100%">
+                  <div class="text-left">
+                    {{
+                      props.row.notes === "" ? "Pas de notes." : props.row.notes
+                    }}
+                  </div>
+                </q-td>
+              </q-tr>
+            </template>
+          </q-table>
         </q-card-section>
       </q-card>
     </div>
@@ -58,7 +94,20 @@ const columns = ref([
     align: "left",
     sortable: true,
   },
-  { name: "email", label: "Email", field: "email", sortable: true },
+  {
+    name: "email",
+    label: "Email",
+    field: "email",
+    align: "left",
+    sortable: true,
+  },
+  {
+    name: "address",
+    label: "Adresse",
+    field: "address",
+    align: "center",
+    sortable: true,
+  },
   { name: "phone", label: "Téléphone", field: "phone", sortable: true },
   { name: "city", label: "Ville", field: "city", sortable: true },
 ]);
@@ -68,8 +117,10 @@ const rows = computed(() =>
       name: client.nom,
       contactName: client.nom_contact,
       email: client.email,
+      address: client.adresse,
       phone: client.telephone,
       city: client.ville,
+      notes: client.notes,
     };
   })
 );
